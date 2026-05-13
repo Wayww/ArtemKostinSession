92cb0d29e37d7ff19bed498d80164becf465997c
Wayww <artem.kostin.v@gmail.com>
2026-05-13 14:54:18 +0500

    add user entity

--- src/main/java/com/kostinartem/courseplatform/repository/KostinArtemUserRepository.java
+++ src/main/java/com/kostinartem/courseplatform/repository/KostinArtemUserRepository.java
@@ -1,4 +1,12 @@
 package com.kostinartem.courseplatform.repository;
 
-public class KostinArtemUserRepository {
+import com.kostinartem.courseplatform.entity.KostinArtemUser;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KostinArtemUserRepository extends JpaRepository<KostinArtemUser, Long> {
+
+    Optional<KostinArtemUser> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
