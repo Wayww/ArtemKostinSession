2cd523d091dfa530716e58a9c0b46a837eaed638
Wayww <artem.kostin.v@gmail.com>
2026-05-13 15:14:36 +0500

    add enrollment entity

---
+++ src/main/java/com/kostinartem/courseplatform/repository/KostinArtemEnrollmentRepository.java
@@ -0,0 +1,7 @@
+package com.kostinartem.courseplatform.repository;
+
+import com.kostinartem.courseplatform.entity.KostinArtemEnrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KostinArtemEnrollmentRepository extends JpaRepository<KostinArtemEnrollment, Long> {
+}
